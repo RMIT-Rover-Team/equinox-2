6efd800b70ac2293df0bc9052d974183dbc6ea72
Kaelan <megakg@outlook.com>
2026-02-15 00:20:19 +1100

    Typo

--- build_pycontrol.sh
+++ build_pycontrol.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir dist
 cd src
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd ..
 cp src/torque*.cpython* -v dist/
 cp src/pycontrol/*.py src/pycontrol/*.html -v dist/
