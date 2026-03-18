7cb44617934a4fb405ecf8219a22baefb4ed8eb4
Kaelan <megakg@outlook.com>
2026-03-18 20:48:07 +1100

    working remote

--- test/autonomyPiServer/main.py
+++ test/autonomyPiServer/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import torque
+import StatusIndicator as SI
 import TCPstreams5 as tcp
 import struct
 
@@ -10,14 +11,19 @@ if __name__ == '__main__':
     client = tcp.serverCon(server)
     print("Accept Client")
 
-    myCon = torque.TorqueHandler("vcan0")
+    myCon = torque.TorqueHandler("can0")
     myCon.set_mode(torque.UNLOCKED_VELOCITY)
     myCon.enable()
 
-
+    SI.setLED(SI.LEDCOLOUR.SAFE)
     while True:
         d = client.getdat()
 
         Left, Right = struct.unpack("<dd",d)
         print("Speed",Left,Right)
         myCon.set_speed(Left, Right)
+
+        if (Left != 0) and (Right != 0):
+            SI.setLED(SI.LEDCOLOUR.AUTO)
+        else:
+            SI.setLED(SI.LEDCOLOUR.AUTO_PREP)
