db9c60cafe5c58ba2b51e1359d1eceedbaa42760
JonTran04 <jontran04@gmail.com>
2026-04-26 14:42:13 +1000

    feat: added Git standards page along with standards subdirectory.

--- eq2-docs/astro.config.mjs
+++ eq2-docs/astro.config.mjs
@@ -6,25 +6,37 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'Equinox 2',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                    { label: 'ROS2 Workshop', slug: 'guides/ros2_ws'}
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-		customCss: [
-			'./src/styles/custom.css'
-		],
-		}), mdx()],
+  integrations: [
+    starlight({
+      title: "Equinox 2",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Standards",
+          items: [
+            { label: "Git standards", slug: "standards/git"}
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "ROS2 Workshop", slug: "guides/ros2" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+    mdx(),
+  ],
 });
