6790fd0486eaf579fe975ab017e61fc555370faa
waitsy <39247729+waitsy@users.noreply.github.com>
2026-06-21 12:42:31 +1000

    Create SciencePayload class

---
+++ onboard/hardware-interfaces/science/SciencePayload.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nick Waites on 21/6/2026.
+//
+
+#include "SciencePayload.h"
---
+++ onboard/hardware-interfaces/science/SciencePayload.h
@@ -0,0 +1,12 @@
+#include "GenericCan.h"
+#include "SocketCanWrapper.h"
+#include "RoverCanMaster.h"
+
+#ifndef EQUINOX_2_SCIENCEPAYLOAD_H
+#define EQUINOX_2_SCIENCEPAYLOAD_H
+
+class SciencePayload {
+};
+
+
+#endif //EQUINOX_2_SCIENCEPAYLOAD_H
