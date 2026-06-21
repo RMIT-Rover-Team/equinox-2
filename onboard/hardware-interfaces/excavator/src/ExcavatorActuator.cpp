6b47ccd5f5f27f0dff3f0179cbcbd873717c1366
rileyt101 <riles3398@gmail.com>
2026-06-21 16:09:49 +1000

    fixed format_device_motor_id

--- onboard/hardware-interfaces/excavator/src/ExcavatorActuator.cpp
+++ onboard/hardware-interfaces/excavator/src/ExcavatorActuator.cpp
@@ -4,10 +4,10 @@
  * Returns a 5 bit integer with the first 3 bits being the device id and the last 2 being the motor id
  */
 uint8_t format_device_motor_id(uint8_t device_id, uint8_t motor_id) {
-    return ((device_id && 0b00000111) << 2) && (motor_id && 0b00000011);
+    return ((device_id & 0b00000111) << 2) | (motor_id & 0b00000011);
 }
 
-ExcavatorActuator::ExcavatorActuator(u_int8_t device_id, u_int8_t motor_id, RoverCanMaster &can_master) : device_id(device_id), motor_id(motor_id), velocity(0.0), can_master(can_master) {}
+ExcavatorActuator::ExcavatorActuator(uint8_t device_id, uint8_t motor_id, RoverCanMaster &can_master) : device_id(device_id), motor_id(motor_id), velocity(0.0), can_master(can_master) {}
 
 void ExcavatorActuator::set_velocity(int16_t target_velocity) {
     spdlog::critical("Set velocity of excavator actuator {0:x} to {0:d}", motor_id, target_velocity);
