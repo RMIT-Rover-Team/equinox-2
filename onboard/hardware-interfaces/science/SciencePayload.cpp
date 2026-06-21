14ed6533040951c14004bc58ae1156c66a54f4ef
waitsy <39247729+waitsy@users.noreply.github.com>
2026-06-21 13:07:37 +1000

    Write initial functions for science payload

--- onboard/hardware-interfaces/science/SciencePayload.cpp
+++ onboard/hardware-interfaces/science/SciencePayload.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "SciencePayload.h"
+
+SciencePayload::SciencePayload() {
+}
+
+SciencePayload::~SciencePayload() {
+}
+
+void SciencePayload::set_heater_state(uint8_t temperature) {
+}
+
+void SciencePayload::set_heater_state(bool state) {
+}
+
+void SciencePayload::motor_cycle() {
+}
+
+void SciencePayload::set_servo_position(int device_id, float cm) {
+}
+
+void SciencePayload::set_servo_velocity(int device_id, float cm_per_second) {
+}
+
+void SciencePayload::set_microscope_swivel(int device_id, float degrees) {
+}
+
+void SciencePayload::stop_all() {
+}
--- onboard/hardware-interfaces/science/SciencePayload.h
+++ onboard/hardware-interfaces/science/SciencePayload.h
@@ -6,6 +6,22 @@
 #define EQUINOX_2_SCIENCEPAYLOAD_H
 
 class SciencePayload {
+private:
+    uint8_t heater_temperature;
+    float drill_position;
+    float drill_velocity;
+    float microscope_position;
+    float microscope_rotation;
+public:
+    SciencePayload();
+    ~SciencePayload();
+    void motor_cycle(); // Cycle motor from min to max to find limits
+    void set_heater_state(uint8_t temperature);
+    void set_heater_state(bool state);
+    void set_servo_position(int device_id, float cm);
+    void set_servo_velocity(int device_id, float cm_per_second);
+    void set_microscope_swivel(int device_id, float degrees);
+    void stop_all();
 };
 
 
