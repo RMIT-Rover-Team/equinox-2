adc9e1cfbe64d5c0511303f2bb60673510f44e3d
waitsy <39247729+waitsy@users.noreply.github.com>
2026-06-24 14:52:41 +1000

    feat: Implemented Science hardware interface (#38)

--- onboard/hardware-interfaces/science/src/SciencePayload.cpp
+++ onboard/hardware-interfaces/science/src/SciencePayload.cpp
@@ -1,43 +1,11 @@
 #include "SciencePayload.h"
 
-SciencePayload::SciencePayload() {
-}
+SciencePayload::SciencePayload()
+    : can_bus(CAN_BUS_NAME)
+    , can_master(can_bus, GroupId::PAYLOAD)
+    , heater(0x01, can_master)
+    , drill(0x02, can_master)
+    , microscope(0x03, can_master) {}
 
 SciencePayload::~SciencePayload() {
-}
-
-void SciencePayload::set_heater_state(uint8_t temperature) {
-}
-
-void SciencePayload::set_heater_state(bool state) {
-}
-
-void SciencePayload::motor_cycle() {
-}
-
-void SciencePayload::set_servo_position(int device_id, float cm) {
-}
-
-void SciencePayload::set_servo_velocity(int device_id, float cm_per_second) {
-}
-
-void SciencePayload::set_microscope_swivel(int device_id, float degrees) {
-}
-
-void SciencePayload::stop_all() {
-}
-
-int SciencePayload::get_heater_state() {
-}
-
-int SciencePayload::get_heater_current_temperature() {
-}
-
-float SciencePayload::get_servo_position(int device_id) {
-}
-
-float SciencePayload::get_servo_velocity(int device_id) {
-}
-
-float SciencePayload::get_microscope_swivel(int device_id) {
 }
--- onboard/hardware-interfaces/science/src/SciencePayload.h
+++ onboard/hardware-interfaces/science/src/SciencePayload.h
@@ -1,32 +1,25 @@
 #include "../../lib-universal-canbus/libuniversalcan/GenericCan.h"
 #include "../../lib-universal-canbus/libuniversalcan/SocketCanWrapper.h"
 #include "../../lib-universal-canbus/libuniversalcan/RoverCanMaster.h"
+#include "Heater.h"
+#include "Drill.h"
+#include "Microscope.h"
 
 #ifndef EQUINOX_2_SCIENCEPAYLOAD_H
 #define EQUINOX_2_SCIENCEPAYLOAD_H
 
+#define CAN_BUS_NAME "can0"
+
 class SciencePayload {
 private:
-    uint8_t heater_temperature;
-    float drill_position;
-    float drill_velocity;
-    float microscope_position;
-    float microscope_rotation;
+    WrappedCANBus can_bus;
+    RoverCanMaster can_master;
+    Heater heater;
+    Drill drill;
+    Microscope microscope;
 public:
     SciencePayload();
     ~SciencePayload();
-    void motor_cycle(); // Cycle motor from min to max to find limits
-    void set_heater_state(uint8_t temperature);
-    void set_heater_state(bool state);
-    int get_heater_state();
-    int get_heater_current_temperature();
-    void set_servo_position(int device_id, float cm);
-    float get_servo_position(int device_id);
-    void set_servo_velocity(int device_id, float cm_per_second);
-    float get_servo_velocity(int device_id);
-    void set_microscope_swivel(int device_id, float degrees);
-    float get_microscope_swivel(int device_id);
-    void stop_all();
 };
 
 
