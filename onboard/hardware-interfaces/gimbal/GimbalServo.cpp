bcccc46de949452b3f21767a1c229dc12bd972dc
rileyt101 <riles3398@gmail.com>
2026-06-28 14:45:32 +1000

    feat: implented set_gimbal_movement, estop functions (#101)

--- onboard/hardware-interfaces/gimbal/GimbalServo.cpp
+++ onboard/hardware-interfaces/gimbal/GimbalServo.cpp
@@ -1,1 +1,24 @@
-#include "GimbalServo.h" 
+#include "GimbalServo.h"
+
+GimbalServo::GimbalServo(uint8_t device_id, RoverCanMaster &can_master) : device_id(device_id), can_master(can_master), tilt_speed(0.0), pan_position(0.0) {}
+
+void GimbalServo::set_gimbal_movement(int8_t target_tilt_speed, int8_t target_pan_position) {
+    spdlog::critical("Set gimbal servo {0:x} tilt speed to {1:d} and pan position to {2:d}", device_id, target_tilt_speed, target_pan_position);
+
+    tilt_speed = target_tilt_speed;
+    pan_position = target_pan_position;
+
+    int8_t msg[8] = { tilt_speed, pan_position, 0, 0, 0, 0, 0, 0 };
+    can_master.tx_int8(GroupId::ONBOARD, device_id, msg);
+}
+
+void GimbalServo::estop() {
+    spdlog::critical("ESTOP GIMBAL SERVO {0:x}", device_id);
+
+    tilt_speed = 0.0;
+    pan_position = 0.0;
+    
+    can_master.estop(GroupId::ONBOARD, device_id);
+}
+
+GimbalServo::~GimbalServo() {}
--- onboard/hardware-interfaces/gimbal/GimbalServo.h
+++ onboard/hardware-interfaces/gimbal/GimbalServo.h
@@ -2,18 +2,18 @@
 #include <stdint.h>
 #include "../GenericCan.h"
 #include "../lib-universal-canbus/libuniversalcan/RoverCanMaster.h"
+#include "spdlog/spdlog.h"
 
 class GimbalServo {
 private:
     uint8_t device_id;
-    int16_t tiltSpeed;
-    int16_t panSpeed;
+    int8_t tilt_speed;
+    int8_t pan_position;
     RoverCanMaster &can_master; // can only have 1 master per bus, so use a ptr
 
 public:
     GimbalServo(uint8_t device_id, RoverCanMaster &can_master);
     ~GimbalServo();
-    void set_tilt_speed(int16_t target_tilt_speed);
-    void set_pan_speed(int16_t target_pan_speed);
+    void set_gimbal_movement(int8_t target_tilt_speed, int8_t target_pan_position);
     void estop();
 };
