75c345cff2871c7565d1b2ae52ffc6472515216e
Ayla Oliver <zimbobub@protonmail.com>
2026-07-02 19:59:00 +1000

    add paver magnet to estop method

--- onboard/hardware-interfaces/excavator/src/ExcavatorPayload.cpp
+++ onboard/hardware-interfaces/excavator/src/ExcavatorPayload.cpp
@@ -1,7 +1,7 @@
 #include "ExcavatorPayload.h"
 
 ExcavatorPayload::ExcavatorPayload()
-    : can_bus(CAN_BUS_NAME)                 // initialize first, order matters
+    : can_bus(CAN_BUS_NAME)
     , can_master(can_bus, GroupId::PAYLOAD)
     , excavator_tilt(DeviceId::EXCAVATOR_TILT, can_master)
     , bucket_tilt(DeviceId::BUCKET_TILT, can_master)
@@ -12,6 +12,7 @@ void ExcavatorPayload::estop() {
     excavator_tilt.estop();
     bucket_tilt.estop();
     teeth.estop();
+    paver_magnet.set_status(false);
 }
 
 ExcavatorPayload::~ExcavatorPayload() {}
