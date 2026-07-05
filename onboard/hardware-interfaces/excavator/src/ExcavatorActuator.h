c0fffc305c837011168883a01b2cf7806362f8d9
Nitya Mungalpara <nitya25mungalpara@gmail.com>
2026-07-05 13:17:16 +1000

    Got rid of motor_id

--- onboard/hardware-interfaces/excavator/src/ExcavatorActuator.h
+++ onboard/hardware-interfaces/excavator/src/ExcavatorActuator.h
@@ -8,7 +8,7 @@ class ExcavatorActuator {
 protected:
     uint8_t device_id;
     int16_t velocity;
-    RoverCanMaster &can_master; // can only have 1 master per bus, so use a ptr
+    RoverCanMaster &can_master; // one master per bus; held by reference, not owned
 
 public:
     // note that device id and motor id have been merged in the docs, discuss changing to 1 id param as well
