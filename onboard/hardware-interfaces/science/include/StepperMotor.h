b177c91dcc8a135fa89b0b9a5ac5d733e5f9c928
waitsy <39247729+waitsy@users.noreply.github.com>
2026-07-12 16:10:38 +1000

    refactor: CANBus communication now in seperate thread (#38)

--- onboard/hardware-interfaces/science/include/StepperMotor.h
+++ onboard/hardware-interfaces/science/include/StepperMotor.h
@@ -7,17 +7,26 @@
 #define EQUINOX_2_STEPPERMOTOR_H
 
 #include "../../lib-universal-canbus/libuniversalcan/RoverCanMaster.h"
+#include <thread>
 
 class StepperMotor {
 private:
     uint8_t device_id;
     RoverCanMaster &can_master;
-    int16_t steps;
-    int16_t last_command_steps;
+    int16_t current_steps;
+    int16_t target_steps;
+    bool target_changed;
+
+    std::mutex motor_mutex;
+    std::atomic<bool> stop_can_worker;
+    std::thread can_worker_thread;
+
+    void can_monitor_loop();
 public:
     StepperMotor(uint8_t device_id, RoverCanMaster &can_master);
+    ~StepperMotor();
     void set_steps(int16_t steps);
-    int get_steps();
+    int get_steps() const;
     void stop();
 };
 
--- onboard/hardware-interfaces/science/src/StepperMotor.cpp
+++ onboard/hardware-interfaces/science/src/StepperMotor.cpp
@@ -9,18 +9,54 @@
 
 StepperMotor::StepperMotor(uint8_t device_id, RoverCanMaster &can_master)
     : device_id(device_id)
-    , can_master(can_master) {}
-
-void StepperMotor::set_steps(int16_t steps) {
-    last_command_steps = steps;
-    int16_t message[4] = {steps, 0, 0, 0};
-    can_master.tx_int16(GroupId::PAYLOAD, device_id, message);
+    , can_master(can_master)
+    , current_steps(0)
+    , target_steps(0)
+    , target_changed(false)
+    , stop_can_worker(false)
+{
+    can_worker_thread = std::thread(&StepperMotor::can_monitor_loop, this);
 }
 
-int StepperMotor::get_steps() {
-    return steps;
+StepperMotor::~StepperMotor() {
+    stop_can_worker = true;
+    if (can_worker_thread.joinable())
+        can_worker_thread.join();
+}
+
+void StepperMotor::set_steps(int16_t steps) {
+    std::lock_guard<std::mutex> lock(motor_mutex);
+    target_steps = steps;
+    target_changed = true;
+}
+
+int StepperMotor::get_steps() const {
+    return current_steps;
 }
 
 void StepperMotor::stop() {
-    last_command_steps = 0;
+    target_steps = 0;
+}
+
+void StepperMotor::can_monitor_loop() {
+    while (!stop_can_worker) {
+        int16_t local_target_steps = 0;
+        bool should_update = false;
+
+        {
+            std::lock_guard<std::mutex> lock(motor_mutex);
+            if (target_changed) {
+                local_target_steps = target_steps;
+                should_update = true;
+                target_changed = false;
+            }
+        }
+
+        if (should_update) {
+            int16_t message[4] = {local_target_steps, 0, 0, 0};
+            can_master.tx_int16(GroupId::PAYLOAD, device_id, message);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
 }
