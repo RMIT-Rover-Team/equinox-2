bf3d6c19e90946670c4a329d891b870174f0eaef
waitsy <39247729+waitsy@users.noreply.github.com>
2026-07-17 17:43:09 +1000

    feat: Added Drill tests (#117)

--- onboard/hardware-interfaces/science/tests/DrillTests.cpp
+++ onboard/hardware-interfaces/science/tests/DrillTests.cpp
@@ -1,9 +1,49 @@
-#include "SciencePayload.h"
+#include "Drill.h"
 #include <gtest/gtest.h>
 
-TEST(DrillTest, SetHeight) {
-    #define CAN_BUS_NAME = "can0";
-    WrappedCANBus can_bus(CAN_BUS_NAME);
-    RoverCanMaster can_master(can_bus, GroupId::PAYLOAD);
-    Drill drill(0x02, can_master);
+#include "SciencePayload.h"
+#include "SocketCanWrapper.h"
+
+auto* drill_test_can_bus = new WrappedCANBus(CAN_BUS_NAME);
+auto* drill_test_can_master = new RoverCanMaster(*drill_test_can_bus, 1);
+
+struct DrillTest : ::testing::Test, public Drill {
+    DrillTest() : Drill(1, *drill_test_can_master) {};
+};
+
+TEST_F(DrillTest, CurrentRpmInitialisesZero) {
+    EXPECT_EQ(0, getCurrentRpm());
+}
+
+TEST_F(DrillTest, CurrentHeightInitialisesZero) {
+    EXPECT_EQ(0, getCurrentHeight());
+}
+
+TEST_F(DrillTest, TargetHeightInitialisesZero) {
+    EXPECT_EQ(0, getTargetHeight());
+};
+
+TEST_F(DrillTest, CanSetTargetHeight) {
+    setHeight(100);
+    EXPECT_EQ(100, getTargetHeight());
+    setHeight(-100);
+    EXPECT_EQ(-100, getTargetHeight());
+}
+
+TEST_F(DrillTest, TargetRpmInitialisesZero) {
+    EXPECT_EQ(0, getTargetRpm());
+}
+
+TEST_F(DrillTest, CanSetTargetRpm) {
+    start(100);
+    EXPECT_EQ(100, getTargetRpm());
+    start();
+    EXPECT_EQ(DRILL_RUNNING_RPM, getTargetRpm());
+}
+
+TEST_F(DrillTest, CanStopDrill) {
+    start(100);
+    EXPECT_NE(0, getTargetRpm());
+    stop();
+    EXPECT_EQ(0, getTargetRpm());
 }
