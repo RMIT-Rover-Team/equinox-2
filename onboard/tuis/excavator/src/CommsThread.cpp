1e8696c1b181dd48da0a3e24401bf27a1e725e22
Ayla Oliver <zimbobub@protonmail.com>
2026-07-24 14:15:44 +1000

    more tui basics done

---
+++ onboard/tuis/excavator/src/CommsThread.cpp
@@ -0,0 +1,67 @@
+#include "CommsThread.h"
+
+
+CommsThread::CommsThread() : can_bus("can2"), can_master(can_bus, 0x0), excavator(can_master) {}
+CommsThread::~CommsThread() {}
+
+bool CommsThread::excavator_alive() {
+    return excavator_tilt_alive;
+}
+
+bool CommsThread::bucket_alive() {
+    return bucket_tilt_alive;
+}
+
+void CommsThread::estop() {
+    excavator.estop();
+}
+
+void CommsThread::set_excavator_velocity(int16_t velocity) {
+    std::lock_guard<std::mutex> lock(m_target_velocity);
+    excavator_target_velocity = velocity;
+}
+
+void CommsThread::set_bucket_velocity(int16_t velocity) {
+    std::lock_guard<std::mutex> lock(m_target_velocity);
+    bucket_target_velocity = velocity;
+}
+
+
+void CommsThread::start() {
+    worker = std::thread(&CommsThread::run, this);
+}
+
+/// @brief Kills the worker thread. Note that it is blocking until the worker next reads the command queue
+void CommsThread::stop() {
+    std::lock_guard<std::mutex> lock(m_should_stop);
+    should_stop = true;
+    if (worker.joinable()) worker.join();
+}
+
+/// @brief  Ran on the worker thread
+void CommsThread::run() {
+    while (true) {
+        {
+            std::lock_guard<std::mutex> lock(m_should_stop);
+            if (should_stop) break;
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(m_target_velocity);
+
+            if (excavator_target_velocity != last_sent_excavator_velocity) {
+                excavator.excavator_tilt.set_velocity(excavator_target_velocity);
+                last_sent_excavator_velocity = excavator_target_velocity;
+            }
+
+            if (bucket_target_velocity != last_sent_bucket_velocity) {
+                excavator.bucket_tilt.set_velocity(bucket_target_velocity);
+                last_sent_bucket_velocity = bucket_target_velocity;
+            }
+        }
+
+        // TODO: change to conditional variable
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    
+}
---
+++ onboard/tuis/excavator/src/CommsThread.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <atomic>
+#include <queue>
+#include "ExcavatorPayload.h"
+#include "RoverCanMaster.h"
+#include "SocketCanWrapper.h"
+
+
+enum ThreadCommand {
+    STOP
+};
+
+
+class CommsThread {
+private:
+    std::thread worker;
+    // worker thread only
+    WrappedCANBus can_bus;
+    RoverCanMaster can_master;
+    ExcavatorPayload excavator;
+
+    /// if != target, need to send new packet
+    int16_t last_sent_excavator_velocity;
+    int16_t last_sent_bucket_velocity;
+
+    // both worker and main thread
+    std::mutex m_target_velocity;
+    int16_t excavator_target_velocity;
+    int16_t bucket_target_velocity;
+
+    std::mutex m_should_stop;
+    bool should_stop;
+
+    // true if recieved a ping from the respective device
+    std::atomic_bool excavator_tilt_alive{false};
+    std::atomic_bool bucket_tilt_alive{false};
+
+    void run();
+public:
+    CommsThread();
+    ~CommsThread();
+
+    void start();
+    void stop();
+
+    bool excavator_alive();
+    bool bucket_alive();
+
+    void set_excavator_velocity(int16_t velocity);
+    void set_bucket_velocity(int16_t velocity);
+    void estop();
+};
