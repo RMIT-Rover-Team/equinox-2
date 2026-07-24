619669bd7c26896ceeaf98f5c7d0fb023c9e6048
Ayla Oliver <zimbobub@protonmail.com>
2026-07-24 14:34:34 +1000

    update getch

--- onboard/tuis/excavator/src/ExcavatorTUI.cpp
+++ onboard/tuis/excavator/src/ExcavatorTUI.cpp
@@ -25,24 +25,27 @@ int main() {
 
 
 // https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+// https://www.man7.org/linux/man-pages/man3/termios.3.html
 char getch() {
         char buf = 0;
         struct termios old = {0};
 
-        if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
+        if (tcgetattr(0, &old) < 0) perror("Error reading termios attributes (tcsetattr())");
 
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
+        old.c_lflag &= ~ICANON; // disable canonical mode (input avaliable immediately, dont wait for newline)
+        old.c_lflag &= ~ECHO;   // stop echo
 
-        if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0) perror("read()");
+        old.c_cc[VMIN] = 1;     // block until char to read
+        old.c_cc[VTIME] = 0;    // no timeout
 
+        if (tcsetattr(0, TCSANOW, &old) < 0) perror("Error setting terminal to non-canonical mode (tcsetattr !ICANON)");
+        if (read(0, &buf, 1) < 0) perror("Error reading char");
+
+        // reset
         old.c_lflag |= ICANON;
         old.c_lflag |= ECHO;
 
-        if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("tcsetattr ~ICANON");
+        if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("Error resetting terminal to canonical mode (tcsetattr ICANON)");
 
         return buf;
 }
