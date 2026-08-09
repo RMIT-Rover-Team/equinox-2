be547c7478e7e9f9b9e794064fdb684e6af076f6
Ayla Oliver <zimbobub@protonmail.com>
2026-08-09 07:14:54 +0000

    clean up gimbal hardware interface

--- onboard/hardware-interfaces/gimbal/src/Gimbal.h
+++ onboard/hardware-interfaces/gimbal/src/Gimbal.h
@@ -1,12 +1,19 @@
 #pragma once
 
-#include "GimbalServo.h"
+#include <stdint.h>
 #include "GenericCan.h"
+#include "RoverCanMaster.h"
+#include "spdlog/spdlog.h"
 
+// Controls the gimbal servo via CAN bus
+// Handles tilt speed and pan position for camera movement
 class Gimbal {
 private:
-    RoverCanMaster can_master;
-    GimbalServo servo;
+    RoverCanMaster &can_master;
+    uint8_t device_id = 0;
+
+    int8_t tilt_speed = 0;
+    int8_t pan_position = 0;
 
 public:
     Gimbal(RoverCanMaster& can_master);
@@ -15,4 +22,6 @@ public:
     void set_tilt_speed(int8_t speed);
     void set_pan_position(int8_t position);
     void set_tilt_and_pan(int8_t target_tilt_speed, int8_t target_pan_position);
+    int8_t get_tilt_speed();
+    int8_t get_pan_position();
 };
--- onboard/hardware-interfaces/gimbal/test/GimbalTest.cpp
+++ onboard/hardware-interfaces/gimbal/test/GimbalTest.cpp
@@ -13,6 +13,26 @@ TEST_F(VCANTest, EStop) {
     EXPECT_EQ(msgs[0].cmd, CommandId::ESTOP);
 }
 
+TEST_F(VCANTest, EStopResetsTiltSpeed) {
+    gimbal.set_tilt_and_pan(50, -30);
+    gimbal.estop();
+
+    read_from_vcan();
+    EXPECT_EQ(msgs_recieved, 2);
+
+    EXPECT_EQ(msgs[0].group_id, GroupId::ONBOARD);
+    EXPECT_EQ(msgs[0].device_id, 0x0);
+    EXPECT_EQ(msgs[0].cmd, CommandId::TXINT8);
+    EXPECT_EQ((int8_t)msgs[0].data[0], 50);
+    EXPECT_EQ((int8_t)msgs[0].data[1], -30);
+
+    EXPECT_EQ(msgs[1].group_id, GroupId::ONBOARD);
+    EXPECT_EQ(msgs[1].device_id, 0x0);
+    EXPECT_EQ(msgs[1].cmd, CommandId::ESTOP);
+
+    EXPECT_EQ(gimbal.get_tilt_speed(), 0);
+    EXPECT_EQ(gimbal.get_pan_position(), -30);
+}
 
 TEST_F(VCANTest, TiltAndPanSavedBetweenMsgs) {
     gimbal.set_tilt_speed(50);
@@ -25,23 +45,23 @@ TEST_F(VCANTest, TiltAndPanSavedBetweenMsgs) {
     EXPECT_EQ(msgs[0].group_id, GroupId::ONBOARD);
     EXPECT_EQ(msgs[0].device_id, 0x0);
     EXPECT_EQ(msgs[0].cmd, CommandId::TXINT8);
-    EXPECT_EQ(msgs[0].data[0], 50);
-    EXPECT_EQ(msgs[0].data[1], 0);
+    EXPECT_EQ((int8_t)msgs[0].data[0], 50);
+    EXPECT_EQ((int8_t)msgs[0].data[1], 0);
 
     EXPECT_EQ(msgs[1].group_id, GroupId::ONBOARD);
     EXPECT_EQ(msgs[1].device_id, 0x0);
     EXPECT_EQ(msgs[1].cmd, CommandId::TXINT8);
-    EXPECT_EQ(msgs[1].data[0], 50);
-    EXPECT_EQ(msgs[1].data[1], -30);
+    EXPECT_EQ((int8_t)msgs[1].data[0], 50);
+    EXPECT_EQ((int8_t)msgs[1].data[1], -30);
 
     EXPECT_EQ(msgs[2].group_id, GroupId::ONBOARD);
     EXPECT_EQ(msgs[2].device_id, 0x0);
     EXPECT_EQ(msgs[2].cmd, CommandId::TXINT8);
-    EXPECT_EQ(msgs[2].data[0], 10);
-    EXPECT_EQ(msgs[2].data[1], -30);
+    EXPECT_EQ((int8_t)msgs[2].data[0], 10);
+    EXPECT_EQ((int8_t)msgs[2].data[1], -30);
 
-    EXPECT_EQ(gimbal.servo.get_tilt_speed(), 10);
-    EXPECT_EQ(gimbal.servo.get_pan_position(), -30);
+    EXPECT_EQ(gimbal.get_tilt_speed(), 10);
+    EXPECT_EQ(gimbal.get_pan_position(), -30);
 }
 
 TEST_F(VCANTest, SetBothSendsOneMsg) {
@@ -53,10 +73,10 @@ TEST_F(VCANTest, SetBothSendsOneMsg) {
     EXPECT_EQ(msgs[0].group_id, GroupId::ONBOARD);
     EXPECT_EQ(msgs[0].device_id, 0x0);
     EXPECT_EQ(msgs[0].cmd, CommandId::TXINT8);
-    EXPECT_EQ(msgs[0].data[0], 50);
-    EXPECT_EQ(msgs[0].data[1], -30);
+    EXPECT_EQ((int8_t)msgs[0].data[0], 50);
+    EXPECT_EQ((int8_t)msgs[0].data[1], -30);
 
-    EXPECT_EQ(gimbal.servo.get_tilt_speed(), 50);
-    EXPECT_EQ(gimbal.servo.get_pan_position(), -30);
+    EXPECT_EQ(gimbal.get_tilt_speed(), 50);
+    EXPECT_EQ(gimbal.get_pan_position(), -30);
 }
 
