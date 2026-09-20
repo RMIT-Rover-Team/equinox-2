361297783044d5f1a3a3c56e4de8930df6915b7b
JonTran04 <jontran04@gmail.com>
2026-09-20 19:06:49 +1000

    feat: added smooth scrolling #135

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -85,6 +85,7 @@ export default defineConfig({
       ],
       customCss: ["./src/styles/custom.css"],
       components: {
+        Head: "./src/components/head.astro",
         Sidebar: "./src/components/sidebar.astro",
       },
     }),
